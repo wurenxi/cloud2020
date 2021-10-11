72f54e51071ea306770d6cd7d042860515a4439d
GaoFy <GaoFy@qq.com>
2021-10-11 10:16:05 +0800

    code commit

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: gxl
+ * @createDate: 2021/10/11 9:52
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @description:
+ * @author: gxl
+ * @createDate: 2021/10/11 8:50
+ */
+@Slf4j
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper:"+serverPort+"\t"
+                + UUID.randomUUID().toString().substring(0,4);
+    }
+}
