e6b2aac7cb7fd49ee83ef42adfa052ceaa69bf25
GaoFy <GaoFy@qq.com>
2021-10-15 18:21:25 +0800

    code commit

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: gxl
+ * @createDate: 2021/10/15 15:15
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
---
+++ cloud-provider-payment8001/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @description:
+ * @author: gxl
+ * @createDate: 2021/10/15 17:16
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj); //2021-10-15T17:25:38.313+08:00[Asia/Shanghai]
+    }
+}
